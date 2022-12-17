02614ac71ef2d06a07b3a83cc21ecbf64f20a781
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-17 16:46:50 -0300

    tema

--- menu/menu.py
+++ menu/menu.py
@@ -1,7 +1,12 @@
 from PySimpleGUI import PySimpleGUI as sg
 
+sg.theme('DarkBlue3')
 class Banco:
+    def __init__(self) -> None:
+        pass
     #login
-    login = [
+    layout = [
+        [sg.Button('Login')],
+        [sg.Button('Cadastra-se')]
 
     ]
